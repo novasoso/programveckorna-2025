d09e728bdd9eb6f1df3ce898c460f2aba919e5c8
novasoso <jaghatarpaj@gmail.com>
2025-01-09 15:41:55 +0100

    Update NPC1DialogueText.cs

--- Assets/NPC1DialogueText.cs
+++ Assets/NPC1DialogueText.cs
@@ -1,17 +1,24 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class NPC1DialogueText : MonoBehaviour
 {
-    string skib = "skibidi";
+    private checkInteraction Interaction;
+    int trackDialogue = 0;
     // Start is called before the first frame update
     void Start()
     {
-        print(skib);
+        Interaction = GameObject.FindGameObjectWithTag("Player").GetComponent<checkInteraction>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Dialogue()
+    {
+        while I 
     }
 }
