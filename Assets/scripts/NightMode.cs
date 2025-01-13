9aca7444116194c643c4a42b219c6e1526f7a67e
novasoso <jaghatarpaj@gmail.com>
2025-01-13 22:59:11 +0100

    skibidi

--- Assets/scripts/NightMode.cs
+++ Assets/scripts/NightMode.cs
@@ -16,7 +16,7 @@ public class NightMode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        dayMode();
+        nightMode();
     }
 
     private void nightMode()
