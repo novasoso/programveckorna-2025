810d0d61ce33af4569cfc1301b018de644613bd9
idkman-goob <leon.bergestra@elev.ga.lbs.se>
2025-01-13 11:12:45 +0100

    oi prqay that these changes wont destroy our thing hehehe ben
    i make daynight cycle go sigma and whatnot

--- Assets/scripts/NightMode.cs
+++ Assets/scripts/NightMode.cs
@@ -10,6 +10,9 @@ public class NightMode : MonoBehaviour
     public Color dayHue;
     public bool isNight = false;
     public List<GameObject> gameObjects;
+    public float cycle = 0f;
+    public int day = 0;
+    public int night = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,7 @@ public class NightMode : MonoBehaviour
 
     private void nightMode()
     {
+        night = 1;
         gameObjects = new List<GameObject>(GameObject.FindObjectsOfType<GameObject>());
         isNight = true;
         foreach (GameObject obj in gameObjects)
@@ -53,6 +57,7 @@ public class NightMode : MonoBehaviour
 
     private void dayMode()
     {
+        day = 1;
         gameObjects = new List<GameObject>(GameObject.FindObjectsOfType<GameObject>());
         isNight = false;
         foreach (GameObject obj in gameObjects)
@@ -78,6 +83,23 @@ public class NightMode : MonoBehaviour
             {
                 spriteRenderer.color = dayHue;
                 print(spriteRenderer.color);
+            }
+        }
+    }
+    void Update()
+    {
+        cycle += Time.deltaTime;
+        if (cycle >= 360)
+        {
+            cycle = 0;
+            if (day == 1 && night == 0)
+            {
+                nightMode();
+                day = 0;
+            }else if (night == 1 && day == 0)
+            {
+                dayMode();
+                night = 0;
             }
         }
     }
