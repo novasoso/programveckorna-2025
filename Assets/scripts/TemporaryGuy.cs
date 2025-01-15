b713685728bc69e4c79ed09d759c02c201a3cd1e
novasoso <jaghatarpaj@gmail.com>
2025-01-15 16:00:40 +0100

    emil hjälp

--- Assets/scripts/TemporaryGuy.cs
+++ Assets/scripts/TemporaryGuy.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,7 +6,7 @@ public class TemporaryGuy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
