aea63e92b97792bde43518d1b8dec2214b6a5c71
novasoso <jaghatarpaj@gmail.com>
2025-01-24 14:55:23 +0100

    update mursushoploguic

--- Assets/shopMursuLogic.cs
+++ Assets/shopMursuLogic.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class shopMursuLogic : MonoBehaviour
 {
+    shopArrowSelector otherMursu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        otherMursu = FindObjectOfType<shopArrowSelector>().GetComponent<shopArrowSelector>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (otherMursu.inSubMenu ==true && otherMursu.inShop == true)
+        {
+            
+        }
     }
 }
