f79c398b300b2e9a294306338d472f5d192ef881
novasoso <jaghatarpaj@gmail.com>
2025-01-28 14:21:41 +0100

    lick

--- Assets/scripts/DialogBoxController.cs
+++ Assets/scripts/DialogBoxController.cs
@@ -9,6 +9,7 @@ public class DialogBoxController : MonoBehaviour
     public GameObject box;
     playerDetecter detector;
     public float moveBox;
+    bool boxInView;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +20,17 @@ public class DialogBoxController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print("hello!!11");
         if (detector.PlayerDetected == true && Input.GetKeyDown(KeyCode.E))
         {
+            print("moved the bawcks");
+            boxInView = true;
             box.transform.position = new(box.transform.position.x, 160, box.transform.position.z);
         }
-        else if (detector.PlayerDetected == false)
+        else if (detector.PlayerDetected == false && boxInView == false)
         {
+            print("cant fidn u buddy");
+            boxInView = false;
             box.transform.position = new Vector3(box.transform.position.x, -225, box.transform.position.z);
         }
     }
